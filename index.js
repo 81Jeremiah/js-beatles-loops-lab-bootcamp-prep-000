5da41d7cd2f9327e140d930b7ca1bdf78ebdfe52
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-02-04 23:28:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians,instruments){
+  const array =[];
+  for (let i = 0,j= musicians.length;i<j;i++){
+array.push (`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+  }
+  function johnLennonFacts(facts){
+    const exclamationPoints = [];
+    let i = 0;
+    while (i < facts.length){
+      exclamationPoints.push (`${facts[i]}!!!`);
+      i++;
+    }
+    return exclamationPoints;
+  }
+function iLoveTheBeatles(n) {
+  const love =[];
+  do {
+  love.push ("I love the Beatles");
+} while (n < 15);
+return love
+}
+  
+  
+  
